460104f566d086863bab5678b722480be0d1cf4d
Kai-Ming Tu <tutu522@gmail.com>
2010-09-25 12:33:44 +0800

    Finished!
    Bin results are output to a file.

--- src/binning.f90
+++ src/binning.f90
@@ -7,12 +7,14 @@ PROGRAM binning
   INTEGER, DIMENSION(:), ALLOCATABLE :: input_fileid
   CHARACTER(LEN=128), DIMENSION(:), ALLOCATABLE :: input_filename
   CHARACTER(LEN=128) :: command, arg, usage
-  INTEGER :: num_arg, stat, i, num_bins
-  REAL(KIND=8) :: datum, max_dataum, min_dataum, bin_max, bin_min, bin_width
+  INTEGER :: num_arg, stat, i, num_bins, bin_index, bin_overflow_count
+  REAL(KIND=8) :: datum, datum_max, datum_min, bin_max, bin_min, bin_width
   INTEGER, DIMENSION(:), ALLOCATABLE :: bin
+  CHARACTER(LEN=*), PARAMETER :: output_filename = "bin_results.txt"
+  INTEGER, PARAMETER :: output_fileid = 10
 
   call GET_COMMAND_ARGUMENT(NUMBER=0, VALUE=command)
-  usage = "usage: " // TRIM(ADJUSTL(command)) // " <number of input files>&                   
+  usage = "Usage: " // TRIM(ADJUSTL(command)) // " <number of input files>&                   
           & <file1> <file2> ... <number of bins> [<bin min> <bin max>]"
   
   !-- read parameters from command line arguments --!
@@ -32,6 +34,7 @@ PROGRAM binning
   if (num_arg - 2 /= num_input_files .AND. num_arg - 4 /= num_input_files) then
      write(*,*) "Error: number of arguments is not correct."
      write(*,*) "       e.g. number of input files and actual files provided are not the same."
+     write(*,*) usage
      call EXIT(1)
   end if
   write(*,*) "numbmer of input files:", num_input_files
@@ -69,14 +72,14 @@ PROGRAM binning
 
   call get_max_min()
   write(*,*)
-  write(*,*) "min datum:", min_dataum  
-  write(*,*) "max datum:", max_dataum
+  write(*,*) "min datum:", datum_min  
+  write(*,*) "max datum:", datum_max
   write(*,*)
   
   !no data min max provided, use automatically found values
   if (num_arg - 2 == num_input_files) then
-     bin_min = min_dataum
-     bin_max = max_dataum
+     bin_min = datum_min
+     bin_max = datum_max
      write(*,*) "No <bin min> and <bin max> provided."
      write(*,*) "Using Min datum and Max datum as <bin min> and <bin max>."
   else !bin_min and bin_max assigned manually by user
@@ -98,17 +101,45 @@ PROGRAM binning
      write(*,*) "bin maximum:", bin_max
   end if
 
+  bin_width = (bin_max - bin_min) / num_bins
+  
   !start to read and bin the data
+  !binning method is
+  ! 1st   2nd   3rd       Nth
+  !@---@ O---@ O---@ ... O---@
+  bin = 0
+  bin_overflow_count = 0
   do i = 1, num_input_files
-     read(input_fileid(i), *, IOSTAT=stat) datum
-     if (stat > 0) then
-        write(*,*) "Error occurred while reading data from file: "&
-             &//TRIM(ADJUSTL(input_filename(i)))
-        call EXIT(1)
-     else if (stat < 0) then !meet EOF
-        EXIT
-     end if
-     
+     do while (.TRUE.)
+        read(input_fileid(i), *, IOSTAT=stat) datum
+        if (stat > 0) then
+           write(*,*) "Error occurred while reading data from file: "&
+                &//TRIM(ADJUSTL(input_filename(i)))
+           call EXIT(1)
+        else if (stat < 0) then !meet EOF
+           EXIT
+        end if
+        if (datum < bin_min .OR. datum > bin_max) then
+           bin_overflow_count = bin_overflow_count + 1
+        else
+           bin_index = CEILING((datum - bin_min) / bin_width)
+           if (bin_index == 0) then !bin_min belongs to the 1st bin
+              bin_index = 1
+           end if
+           bin(bin_index) = bin(bin_index) + 1
+        end if
+     end do
+  end do
+
+  !output results for drawing histogram
+  open(UNIT=output_fileid, FILE=output_filename, IOSTAT=stat)
+  if (stat /= 0) then
+     write(*,*) "Error: unable to create output file: ", TRIM(ADJUSTL(output_filename))
+     call EXIT(1)
+  end if
+  !the middle value of each bin is bin_min + bin_width*(i-0.5)
+  do i = 1, num_bins
+     write(output_fileid, *) bin_min + bin_width*(i-0.5), bin(i)
   end do
   
 CONTAINS
@@ -116,12 +147,12 @@ CONTAINS
   SUBROUTINE get_max_min()
     IMPLICIT NONE
     
-    read(input_fileid(1), *, IOSTAT=stat) max_dataum
+    read(input_fileid(1), *, IOSTAT=stat) datum_max
     if (stat /= 0) then
        write(*,*) "Error occurred while reading data from file: "//TRIM(ADJUSTL(input_filename(i)))
        call EXIT(1)
     end if
-    min_dataum = max_dataum
+    datum_min = datum_max
     
     do i = 1, num_input_files
        do while (.TRUE.)
@@ -134,10 +165,10 @@ CONTAINS
              REWIND(input_fileid(i))
              EXIT
           end if
-          if (datum > max_dataum) then
-             max_dataum = datum
-          else if (datum < min_dataum) then
-             min_dataum = datum
+          if (datum > datum_max) then
+             datum_max = datum
+          else if (datum < datum_min) then
+             datum_min = datum
           end if
        end do
     end do
